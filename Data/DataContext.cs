e62fa937b19e2afeb09cb67a419ae1d3cee5c3b4
rajat.thapar <rajat.thapar@outlook.com>
2021-09-03 02:09:16 +0530

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using DatinApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatinApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
